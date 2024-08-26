8f31f12483478870fc9be221c79f6cd7aeb68b42
Eduardo Carlos <edu4rdobarb0za@gmail.com>
2024-08-26 19:42:57 -0300

    update: remove unnecessary date methods

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,45 +1,24 @@
 const { format } = require("date-fns");
 const { ptBR } = require("date-fns/locale");
 
-interface IFormattedDate {
-  label: string;
-  date: Date;
-}
-
-interface IFormattedDateRange {
-  startDate: Date;
-  endDate: Date;
-}
-
 interface IDate {
   date: Date;
 }
 
-export const formattedDate = ({ label, date }: IFormattedDate) => {
-  return format(date, label, {
-    locale: ptBR,
-  });
-};
-
-export const formattedDateSimple = ({ date }: IDate) => {
-  return format(date, "dd/MM/yyyy", {
-    locale: ptBR,
-  });
-};
-
-export const formattedHour = ({ date }: IDate) => {
+export const getHour = ({ date }: IDate) => {
   return format(date, "HH:mm'h'", {
     locale: ptBR,
   });
 };
 
-export const formattedDateRange = ({
-  startDate,
-  endDate,
-}: IFormattedDateRange) => {
-  const startDay = format(startDate, "d", { locale: ptBR });
-  const endDay = format(endDate, "d", { locale: ptBR });
-  const month = format(startDate, "MMMM", { locale: ptBR });
+export const getDay = ({ date }: IDate) => {
+  return format(date, "D", {
+    locale: ptBR,
+  });
+};
 
-  return `${startDay} à ${endDay} de ${month}`;
+export const getDayOfWeek = ({ date }: IDate) => {
+  return format(date, "D", {
+    locale: ptBR,
+  });
 };
