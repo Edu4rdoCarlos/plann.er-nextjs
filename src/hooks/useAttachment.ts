d13032703e0793ceda255cdf13cd244a1d502d48
Guylhermee <guylhermee.b@hotmail.com>
2024-08-29 14:41:29 -0300

    feat: add hooks

---
+++ src/hooks/useAttachment.ts
@@ -0,0 +1,51 @@
+
+import { ApiAttachment } from '@/app/services/attachment'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+
+const QUERY_KEY = 'qkAttachment'
+
+const Create = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(ApiAttachment.create, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERY_KEY)
+    }
+  })
+}
+
+const Delete = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(ApiAttachment.delete, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERY_KEY)
+    }
+  })
+}
+
+const ListAll = () => {
+  return useQuery([QUERY_KEY], () => ApiAttachment.listAll())
+}
+
+const FindOne = (id: string) => {
+  return useQuery([QUERY_KEY, id], () => ApiAttachment.findOne(id))
+}
+
+const Update = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(ApiAttachment.update, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERY_KEY)
+    }
+  })
+}
+
+export const useAttachment = {
+    Create,
+    Delete,
+    Update,
+    FindOne,
+    ListAll,
+}
---
+++ src/hooks/useMember.ts
@@ -0,0 +1,50 @@
+import { ApiMember } from '@/app/services/member'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+
+const QUERY_KEY = 'qkMember'
+
+const Create = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(ApiMember.create, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERY_KEY)
+    }
+  })
+}
+
+const Delete = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(ApiMember.delete, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERY_KEY)
+    }
+  })
+}
+
+const ListAll = () => {
+  return useQuery([QUERY_KEY], () => ApiMember.listAll())
+}
+
+const FindOne = (id: string) => {
+  return useQuery([QUERY_KEY, id], () => ApiMember.findOne(id))
+}
+
+const Update = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(ApiMember.update, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(QUERY_KEY)
+    }
+  })
+}
+
+export const useMember = {
+    Create,
+    Delete,
+    Update,
+    FindOne,
+    ListAll,
+}
